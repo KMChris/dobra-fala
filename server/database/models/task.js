56189234c5d9c1ee29dfb351e64d33b7f52297c7
Paweł Duraj <49090420+pawelduraj@users.noreply.github.com>
2023-10-28 19:51:14 +0200

    Add better tasks

--- server/database/models/task.js
+++ server/database/models/task.js
@@ -12,23 +12,43 @@ module.exports = (sequelize) => {
             allowNull: false,
         },
         title: {
-            type: DataTypes.STRING(256),
+            type: DataTypes.STRING(128),
             allowNull: false,
         },
         description: {
             type: DataTypes.TEXT,
             allowNull: false,
         },
+        category: {
+            type: DataTypes.STRING(32),
+            allowNull: false,
+        },
         level: {
             type: DataTypes.INTEGER,
             allowNull: false,
         },
+        min: {
+            type: DataTypes.INTEGER,
+             allowNull: false,
+        },
+        /*geoX: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        geoY: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },*/
         completedBy: {
             type: DataTypes.STRING(16),
             allowNull: true,
         },
         completedAt: {
             type: DataTypes.DATE,
+            allowNull: true,
+        },
+        mark: {
+            type: DataTypes.INTEGER,
             allowNull: true,
         },
     });
